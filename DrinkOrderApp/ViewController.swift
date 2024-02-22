1f53530e0e291bf5beeb36af5907672628fb3210
 Dawei <100706617+dwhao84@users.noreply.github.com>
2024-02-22 15:53:28 +0800

    Initial Commit

---
+++ DrinkOrderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrinkOrderApp
+//
+//  Created by Da-wei Hao on 2024/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
