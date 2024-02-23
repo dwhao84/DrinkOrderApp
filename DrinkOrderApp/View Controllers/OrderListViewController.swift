c1a0f2336918c77ea08c771b0eaf66ec5f7dec32
Daweihao84 <dwsamurai84@gmail.com>
2024-02-23 21:21:22 +0800

    Add the backgroundColor for view.

--- DrinkOrderApp/View Controllers/OrderListViewController.swift
+++ DrinkOrderApp/View Controllers/OrderListViewController.swift
@@ -13,6 +13,7 @@ class OrderListViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = Colors.kebukeLightBlue
     }
     
 
