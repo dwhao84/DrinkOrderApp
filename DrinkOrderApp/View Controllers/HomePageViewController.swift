33733afa6051970f1a28a93e179f0369a27d1fec
Daweihao84 <dwsamurai84@gmail.com>
2024-02-23 21:21:31 +0800

    Add the backgroundColor for view.

--- DrinkOrderApp/View Controllers/HomePageViewController.swift
+++ DrinkOrderApp/View Controllers/HomePageViewController.swift
@@ -13,6 +13,7 @@ class HomePageViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = Colors.kebukeLightBlue
     }
     
 
