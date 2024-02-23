5276fd31b81fec4f165ce8654722efa3eaa7abf6
Daweihao84 <dwsamurai84@gmail.com>
2024-02-23 19:08:56 +0800

    Add Colors constants.

--- DrinkOrderApp/Constants/ColorsConstants.swift
+++ DrinkOrderApp/Constants/ColorsConstants.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 struct Colors {
-    static let kebukeDarkBlue: UIColor   = UIColor(red: 16/255, green: 45/255, blue: 61/255, alpha: 1)
-    static let kebukeLightBlue: UIColor  = UIColor(red: 23/255, green: 61/255, blue: 80/255, alpha: 1)
-    static let kebukeBrown: UIColor      = UIColor(red: 182/255, green: 151/255, blue: 100/255, alpha: 1)
-    static let white: UIColor            = UIColor.white
-    static let darkGrey: UIColor         = UIColor.darkGray
+    // Kebuke Colors:
+    static let kebukeDarkBlue: UIColor  = UIColor(red: 16/255, green: 45/255, blue: 61/255, alpha: 1)
+    static let kebukeLightBlue: UIColor = UIColor(red: 23/255, green: 61/255, blue: 80/255, alpha: 1)
+    static let kebukeBrown: UIColor     = UIColor(red: 182/255, green: 151/255, blue: 100/255, alpha: 1)
+    
+    // System Colors:
+    static let white: UIColor           = UIColor.white
+    static let black: UIColor           = UIColor.black
+    static let darkGray: UIColor        = UIColor.darkGray
+    static let lightGray: UIColor       = UIColor.lightGray
+    static let systemGray2: UIColor     = UIColor.systemGray2
+    static let systemGray3: UIColor     = UIColor.systemGray3
+    static let systemGray4: UIColor     = UIColor.systemGray4
+    static let systemGray5: UIColor     = UIColor.systemGray5
+    static let systemGray6: UIColor     = UIColor.systemGray6
 }
