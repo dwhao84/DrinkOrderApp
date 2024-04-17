920927e28a5badb81ad5e35fcc020b31791344bb
Daweihao84 <dwsamurai84@gmail.com>
2024-04-17 23:24:25 +0800

    Create custom kebuke view.

--- DrinkOrderApp/CustomKebukeView.swift
+++ DrinkOrderApp/CustomKebukeView.swift
@@ -9,12 +9,38 @@ import UIKit
 
 class CustomKebukeView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var bannerImageView: UIImageView = {
+        let imageView: UIImageView = UIImageView()
+        imageView.image = Images.kebukeLogo
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    } ()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addConstrants()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addConstrants () {
+        self.addSubview(bannerImageView)
+        NSLayoutConstraint.activate([
+            bannerImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+            bannerImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            bannerImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            bannerImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+            bannerImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
+    
 }
+
+#Preview(traits: .fixedLayout(width: 428, height: 100), body: {
+    let customKebukeView = CustomKebukeView()
+    return customKebukeView
+   })
