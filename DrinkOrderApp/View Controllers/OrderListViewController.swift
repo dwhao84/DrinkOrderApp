74c86b19e24a986fa86589b1719bd24ae6342c95
Daweihao84 <dwsamurai84@gmail.com>
2024-04-17 23:24:01 +0800

    Add tableView / add tableView delegate & data source.

--- DrinkOrderApp/View Controllers/OrderListViewController.swift
+++ DrinkOrderApp/View Controllers/OrderListViewController.swift
@@ -9,23 +9,43 @@ import UIKit
 
 class OrderListViewController: UIViewController {
 
+    // MARK: - UI set up:
     var tableView: UITableView = {
         let tableView: UITableView = UITableView()
         tableView.backgroundColor = Colors.kebukeLightBlue
-        tableView.estimatedRowHeight = 100
         tableView.allowsSelection = true
+        tableView.rowHeight = 150
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     } ()
     
+    var checkoutView: UIView = {
+        let view: UIView  = UIView()
+        view.backgroundColor = Colors.kebukeLightBlue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    } ()
+    
+    var productImageView: UIImageView = {
+        let imageView: UIImageView = UIImageView()
+        imageView.image = Images.kebukeLogo
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    } ()
+    
+    // MARK: - Life cycle:
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupUI()
         
     }
     
     func setupUI () {
         addConstraints()
+        addDelegateAndDatasource()
+        setupNavigationItem()
     }
     
     func addDelegateAndDatasource () {
@@ -42,8 +62,24 @@ class OrderListViewController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+    
+    func setupNavigationItem () {
+        // set up titleView
+        self.navigationItem.titleView = productImageView
+        self.navigationItem.titleView?.backgroundColor = Colors.kebukeDarkBlue
+        self.navigationController?.navigationBar.backgroundColor = Colors.kebukeDarkBlue
+        self.navigationController?.navigationBar.barTintColor = Colors.kebukeDarkBlue
+        
+        // set up appearance
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = Colors.kebukeDarkBlue
+        self.navigationItem.standardAppearance = appearance
+        self.navigationItem.scrollEdgeAppearance = appearance
+        
+    }
 }
 
+// MARK: - Extension:
 extension OrderListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         1
