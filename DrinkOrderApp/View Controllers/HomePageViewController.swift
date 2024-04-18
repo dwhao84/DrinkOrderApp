f5cf982cf41452d678090bfa1121e4caded48f37
Daweihao84 <dwsamurai84@gmail.com>
2024-04-18 23:50:59 +0800

    Set up HomePageVC.

--- DrinkOrderApp/View Controllers/HomePageViewController.swift
+++ DrinkOrderApp/View Controllers/HomePageViewController.swift
@@ -9,7 +9,11 @@ import UIKit
 
 class HomePageViewController: UIViewController {
  
-    let bannerNameArray: [String] = [ "00", "01_紅玉熟成", "02_芒波&麥雪", "03_茉玉春蘋"]
+    static let shared: String = "HomePageViewController"
+    private let apiKey: String = ""
+    
+    var drinks = [Record] ()
+    var drinksOfSelect = [Record]()
     
     // MARK: - UI set up:
     var productImageView: UIImageView = {
@@ -35,7 +39,6 @@ class HomePageViewController: UIViewController {
         return imageView
     } ()
     
-    
     var pageControl: UIPageControl = {
         let pageControl: UIPageControl = UIPageControl()
         pageControl.direction = .leftToRight
@@ -50,6 +53,14 @@ class HomePageViewController: UIViewController {
         return pageControl
     } ()
     
+    var drinksTableView: UITableView = {
+        let tableView: UITableView = UITableView()
+        tableView.rowHeight = 100
+        tableView.allowsSelection = true
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    } ()
+    
     // MARK: - Life cycle:
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,24 +71,39 @@ class HomePageViewController: UIViewController {
     
     func setupUI () {
         self.view.backgroundColor = Colors.kebukeLightBlue
+        
         setupNavigationItem()
-        addConstrants()
+        
+        addDelegateAndDataSource()
+        configureDrinksTableView()
+        
+        addConstraints()
+
     }
     
     func setupNavigationItem () {
-        // set up titleView
+        // Set up titleView
         self.navigationItem.titleView = productImageView
         self.navigationItem.titleView?.backgroundColor = Colors.kebukeDarkBlue
         
-        // set up appearance
+        // Set up appearance
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = Colors.kebukeDarkBlue
         self.navigationItem.standardAppearance = appearance
         self.navigationItem.scrollEdgeAppearance = appearance
+    }
+    
+    func configureDrinksTableView () {
+        drinksTableView.register(OrderListTableViewCell.self, forCellReuseIdentifier: OrderListTableViewCell.identifier)
         
     }
     
-    func addConstrants () {
+    func addDelegateAndDataSource () {
+        drinksTableView.delegate = self
+        drinksTableView.dataSource = self
+    }
+    
+    func addConstraints () {
         view.addSubview(bannerImageView)
         NSLayoutConstraint.activate([
             bannerImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -86,9 +112,53 @@ class HomePageViewController: UIViewController {
             bannerImageView.heightAnchor.constraint(equalToConstant: view.bounds.width * 0.3)
         ])
     }
+    
+    func fetchDrinksData () {
+        let url = URL(string: "https://api.airtable.com/v0/appS5I28H2YO3bJzv/Kebuke")!
+        var request = URLRequest(url: url)
+        
+        // 搞清出這段程式的意思
+        request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+        
+        // Get the data by using Decoder.
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data else { return }
+            do {
+                let decoder = JSONDecoder()
+                let drink = try decoder.decode(Kebuke.self, from: data)
+                self.drinks = drink.records
+                
+                DispatchQueue.main.async {
+                    
+                    // Add the drinksTableView into the main thread.
+                    self.drinksTableView.reloadData()
+                }
+            } catch {
+                print("DEBUG PRINT: \(error.localizedDescription)")
+            }
+        }.resume()
+    }
 }
 
 // MARK: - Extension:
+extension HomePageViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("\(drinksOfSelect.count)")
+        return drinksOfSelect.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: OrderListTableViewCell.identifier, for: indexPath)
+        let drinks = drinksOfSelect[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("DEBUG PRINT: didSelectRowAt: \(indexPath.row)")
+    }
+}
 
 
 #Preview {
