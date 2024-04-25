b03830b3c63ae39dd368ef8f74ea994e351d586f
Daweihao84 <dwsamurai84@gmail.com>
2024-04-25 23:51:02 +0800

    Import Kingfisher and GET content by networking. Also set up the cell content by using drinkData to get the all the information from Airtable.

--- DrinkOrderApp/View Controllers/HomePageViewController.swift
+++ DrinkOrderApp/View Controllers/HomePageViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import Kingfisher
 
 class HomePageViewController: UIViewController {
  
     static let shared: String = "HomePageViewController"
-    private let apiKey: String = ""
+    let apiKey: String = "patxAQx4KLgwEsh8O.28a883dd0c29a3920aee1cc069fc876738b14186ec8ec2dd07cc762b70497e0c"
     
     var drinks = [Record] ()
-    var drinksOfSelect = [Record]()
+    var drinksOfselectedCategory = [Record]()
     
     // MARK: - UI set up:
     var productImageView: UIImageView = {
@@ -22,13 +23,6 @@ class HomePageViewController: UIViewController {
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
-    } ()
-    
-    var searchController: UISearchController = {
-        let searchController: UISearchController = UISearchController()
-        searchController.searchBar.text = ""
-        searchController.isActive = true
-        return searchController
     } ()
     
     var bannerImageView: UIImageView = {
@@ -42,7 +36,11 @@ class HomePageViewController: UIViewController {
     var pageControl: UIPageControl = {
         let pageControl: UIPageControl = UIPageControl()
         pageControl.direction = .leftToRight
-        pageControl.currentPageIndicatorTintColor = Colors.kebukeBrown
+        pageControl.currentPageIndicatorTintColor = Colors.white
+        pageControl.tintColor = Colors.kebukeBrown
+        pageControl.backgroundStyle = .prominent
+        pageControl.numberOfPages = 5
+        pageControl.currentPage = 0
         
         let timeProgress = UIPageControlTimerProgress(preferredDuration: 3.5)
         pageControl.progress = timeProgress
@@ -55,11 +53,19 @@ class HomePageViewController: UIViewController {
     
     var drinksTableView: UITableView = {
         let tableView: UITableView = UITableView()
-        tableView.rowHeight = 100
         tableView.allowsSelection = true
+        tableView.isScrollEnabled = true
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     } ()
+    
+    var scrollView: UIScrollView = {
+        let scrollView: UIScrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.isScrollEnabled = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
     
     // MARK: - Life cycle:
     override func viewDidLoad() {
@@ -70,96 +76,143 @@ class HomePageViewController: UIViewController {
     }
     
     func setupUI () {
-        self.view.backgroundColor = Colors.kebukeLightBlue
-        
+        self.view.backgroundColor = Colors.white
         setupNavigationItem()
-        
+        setupTableView()
         addDelegateAndDataSource()
-        configureDrinksTableView()
-        
         addConstraints()
-
+        fetchDrinksData()
+    }
+    
+    func setupTableView () {
+        drinksTableView.register(DrinkTableViewCell.self, forCellReuseIdentifier: DrinkTableViewCell.identifier)
+        drinksTableView.rowHeight = 170
+        drinksTableView.allowsSelection = true
     }
     
     func setupNavigationItem () {
-        // Set up titleView
+        // set up titleView
         self.navigationItem.titleView = productImageView
         self.navigationItem.titleView?.backgroundColor = Colors.kebukeDarkBlue
         
-        // Set up appearance
+        // set up appearance
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = Colors.kebukeDarkBlue
         self.navigationItem.standardAppearance = appearance
         self.navigationItem.scrollEdgeAppearance = appearance
     }
     
-    func configureDrinksTableView () {
-        drinksTableView.register(OrderListTableViewCell.self, forCellReuseIdentifier: OrderListTableViewCell.identifier)
-        
-    }
-    
     func addDelegateAndDataSource () {
         drinksTableView.delegate = self
         drinksTableView.dataSource = self
+        scrollView.delegate = self
     }
     
     func addConstraints () {
-        view.addSubview(bannerImageView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(bannerImageView)
+        scrollView.addSubview(pageControl)
+        scrollView.addSubview(drinksTableView)
+        
         NSLayoutConstraint.activate([
-            bannerImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+            bannerImageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
             bannerImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             bannerImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            bannerImageView.heightAnchor.constraint(equalToConstant: view.bounds.width * 0.3)
+            bannerImageView.heightAnchor.constraint(equalToConstant: view.bounds.height / 5),
+
+            pageControl.topAnchor.constraint(equalTo: bannerImageView.bottomAnchor, constant: 70),
+            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pageControl.heightAnchor.constraint(equalToConstant: 20),
+
+            drinksTableView.topAnchor.constraint(equalTo: pageControl.bottomAnchor, constant: 10),
+            drinksTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            drinksTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            drinksTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
     
-    func fetchDrinksData () {
-        let url = URL(string: "https://api.airtable.com/v0/appS5I28H2YO3bJzv/Kebuke")!
+    func fetchDrinksData() {
+        guard let url = URL(string: "https://api.airtable.com/v0/appS5I28H2YO3bJzv/Kebuke") else {
+            print("DEBUG PRINT: Invalid URL")
+            return
+        }
+
         var request = URLRequest(url: url)
-        
-        // 搞清出這段程式的意思
         request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
-        
-        // Get the data by using Decoder.
+
         URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data else { return }
+            if let error = error {
+                print("DEBUG PRINT: Request error: \(error.localizedDescription)")
+                return
+            }
+
+            guard let data = data else {
+                print("DEBUG PRINT: No data received")
+                return
+            }
+
             do {
                 let decoder = JSONDecoder()
-                let drink = try decoder.decode(Kebuke.self, from: data)
-                self.drinks = drink.records
-                
+                let kebuke = try decoder.decode(Kebuke.self, from: data)
+                self.drinks = kebuke.records
+                print("DEBUG PRINT: Drinks loaded successfully with \(self.drinks.count) records.")
+
                 DispatchQueue.main.async {
-                    
-                    // Add the drinksTableView into the main thread.
-                    self.drinksTableView.reloadData()
+                    self.drinksTableView.reloadData()  // Ensure UI updates happen on the main thread
                 }
             } catch {
-                print("DEBUG PRINT: \(error.localizedDescription)")
+                print("Decode error: \(error.localizedDescription)")
+                if let rawJSONString = String(data: data, encoding: .utf8) {
+                    print("DEBUG PRINT: Received JSON - \(rawJSONString)")
+                }
             }
         }.resume()
     }
 }
 
 // MARK: - Extension:
-extension HomePageViewController: UITableViewDelegate, UITableViewDataSource {
+extension HomePageViewController: UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
+    
+    // MARK: - UITableViewDataSource:
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("\(drinksOfSelect.count)")
-        return drinksOfSelect.count
+        print("DEBUG PRINT: 資料為\(drinks.count)")
+        return drinks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: OrderListTableViewCell.identifier, for: indexPath)
-        let drinks = drinksOfSelect[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: DrinkTableViewCell.identifier, for: indexPath) as! DrinkTableViewCell
         
+        cell.selectionStyle = .gray
+        // Define the drinksData from drinks(an array).
+        let drinksData = drinks[indexPath.row]
+        
+        // Using Kingfisher to set the image from URL
+        if let imageUrlString = drinksData.fields.drinksImages?.last?.url, let url = URL(string: imageUrlString) {
+            cell.drinksImageView.kf.setImage(with: url)
+        } else {
+            cell.drinksImageView.image = Images.banner_02 // Set a default image if URL is not available
+        }
+        
+        cell.drinksTitleLabel.text       = drinksData.fields.drinksName
+        cell.drinksDescriptionLabel.text = drinksData.fields.drinksDescription
+        cell.drinksPriceLabel.text       = "中杯:\(drinksData.fields.mediumPrice) / 大杯: \(drinksData.fields.largePrice)"
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        print("DEBUG PRINT: didSelectRowAt: \(indexPath.row)")
+    // MARK: - UITableViewDelegate:
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        print("DEBUG PRINT:\(indexPath.row)")
+        let orderDetailVC = OrderDetailViewController()
+        orderDetailVC.modalPresentationStyle = .popover
+        self.navigationController?.pushViewController(orderDetailVC, animated: true)
     }
 }
-
 
 #Preview {
     UINavigationController(rootViewController: HomePageViewController())
