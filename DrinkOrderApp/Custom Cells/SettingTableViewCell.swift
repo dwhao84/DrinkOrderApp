b126c53214668ad3b93055340efbf9eb29681246
Daweihao84 <dwsamurai84@gmail.com>
2024-04-25 23:48:49 +0800

    Adjust the drinks model.

---
+++ DrinkOrderApp/Custom Cells/SettingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingTableViewCell.swift
+//  DrinkOrderApp
+//
+//  Created by Dawei Hao on 2024/4/25.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- DrinkOrderApp/Models/DrinksModel.swift
+++ DrinkOrderApp/Models/DrinksModel.swift
@@ -7,45 +7,67 @@
 
 import UIKit
 
+// API URL:https://api.airtable.com/v0/appS5I28H2YO3bJzv/Kebuke
+
+// MARK: - Kebuke
 struct Kebuke: Codable {
     let records: [Record]
 }
 
+// MARK: - Record
 struct Record: Codable {
-    let id: String
-    let createdTime: String
+    let id, createdTime: String
     let fields: Fields
 }
 
+// MARK: - Fields
 struct Fields: Codable {
-    let category: String
-    let drinksImages: [DrinksImages]?
-    let drinksName: String
-    let drinksDescription: String
-    let mediumPrice: Int
     let largePrice: Int
-    
+    let drinksImages: [DrinksImage]?
+    let category: Category
+    let mediumPrice: Int
+    let drinksName, drinksDescription: String
+
     enum CodingKeys: String, CodingKey {
-        case category          = "category"
-        case drinksImages      = "Drinks Images"
+        case largePrice = "large price"
+        case drinksImages = "Drinks Images"
+        case category = "Category"
+        case mediumPrice = "medium price"
+        case drinksName = "Drinks name"
         case drinksDescription = "Drinks Description"
-        case drinksName        = "Drinks name"
-        case mediumPrice       = "medium Price"
-        case largePrice        = "large Price"
     }
 }
 
-enum Category: String, CodingKey {
-    case 單品茶                  = "單品茶"
-    case 季節限定                = "季節限定"
-    case 歐蕾MilkTea            = "歐蕾 MilkTea"
-    case 調茶MixTea             = "調茶 MixTea"
-    case 雲蓋SweetCreamColdFoam = "雲蓋 SweetCreamColdFoam"
+enum Category: String, Codable {
+    case 單品茶 = "單品茶"
+    case 季節限定 = "季節限定"
+    case 歐蕾MilkTea = "歐蕾 Milk tea"
+    case 調茶MixTea = "調茶 Mix tea"
+    case 雲蓋SweetCreamColdFoam = "雲蓋 Sweet Cream Cold Foam"
 }
 
-struct DrinksImages: Codable {
+// MARK: - DrinksImage
+struct DrinksImage: Codable {
     let id: String
+    let width, height: Int
     let url: String
+    let filename: String
+    let size: Int
+    let type: TypeEnum
+    let thumbnails: Thumbnails
 }
 
+// MARK: - Thumbnails
+struct Thumbnails: Codable {
+    let small, large, full: Full
+}
 
+// MARK: - Full
+struct Full: Codable {
+    let url: String
+    let width, height: Int
+}
+
+enum TypeEnum: String, Codable {
+    case imageJPEG = "image/jpeg"
+}
