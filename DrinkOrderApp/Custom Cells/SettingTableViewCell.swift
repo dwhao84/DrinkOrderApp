9f65f47291d2b8887e262c606636f9869caa1e37
Daweihao84 <dwsamurai84@gmail.com>
2024-04-27 00:01:53 +0800

    Cut the fetchSettingData, and paste this code snippet into the SettingTableVC.

--- DrinkOrderApp/Custom Cells/SettingTableViewCell.swift
+++ DrinkOrderApp/Custom Cells/SettingTableViewCell.swift
@@ -8,16 +8,58 @@
 import UIKit
 
 class SettingTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "SettingTableViewCell"
+    
+    var serviceImageView: UIImageView = {
+        let imageView: UIImageView = UIImageView()
+        imageView.image = Images.phone
+        imageView.contentMode = .scaleAspectFill
+        imageView.tintColor = Colors.kebukeLightBlue
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    } ()
+    
+    var serviceTitleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "XXXX"
+        label.textColor = Colors.darkGray
+        label.textAlignment = .left
+        label.font = UIFont.boldSystemFont(ofSize: 22)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    } ()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: "drinkCell")
+
+        addConstraints()
+        self.backgroundColor = Colors.white
+     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func addConstraints() {
+        self.addSubview(serviceImageView)
+        self.addSubview(serviceTitleLabel)
+        
+        serviceImageView.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        serviceImageView.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        NSLayoutConstraint.activate([
+            serviceImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            serviceImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            
+            serviceTitleLabel.leadingAnchor.constraint(equalTo: serviceImageView.trailingAnchor, constant: 20),
+            serviceTitleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
     }
 
 }
+
+#Preview(traits: .fixedLayout(width: 428, height: 80), body: {
+    let settingTableViewCell: UITableViewCell = SettingTableViewCell()
+    return settingTableViewCell
+})
--- DrinkOrderApp/Models/SettingsModel.swift
+++ DrinkOrderApp/Models/SettingsModel.swift
@@ -12,18 +12,5 @@ struct SettingData {
     let serviceImage: UIImage
 }
 
-func fetchSettingData () -> [SettingData] {
-    // First group -> Contact Information.
-    let telephoneNumber = SettingData(serviceName: "Telephone Number", serviceImage: Images.phone)
-    let location        = SettingData(serviceName: "Location",         serviceImage: Images.map)
-    let email           = SettingData(serviceName: "E-mail",           serviceImage: Images.mail)
-    
-    // Second group -> Company Information.
-    let declaration     = SettingData(serviceName: "Declaration",      serviceImage: Images.declaration)
-    let inspectReport   = SettingData(serviceName: "Insepect Report",  serviceImage: Images.report)
-    let privacy         = SettingData(serviceName: "Privacy",          serviceImage: Images.privacy)
-    let brandStory      = SettingData(serviceName: "Brand Story",      serviceImage: Images.brandStory)
-    return [telephoneNumber, location, email, declaration, inspectReport, privacy, brandStory]
-}
 
 
