8fba5e7c587097d4fd78fa340f18fea4900a9631
Daweihao84 <dwsamurai84@gmail.com>
2024-04-28 01:39:31 +0800

    Build the KebukeAnnotation to define Kebuke's store info.

--- DrinkOrderApp/KebukeAnnotation.swift
+++ DrinkOrderApp/KebukeAnnotation.swift
@@ -6,7 +6,18 @@
 //
 
 import UIKit
+import MapKit
 
 class KebukeAnnotation: MKPointAnnotation {
-
+    
+    static let identifier: String = "KebukeAnnotation"
+    
+    var image: UIImage?
+    
+    init(coordinate: CLLocationCoordinate2D, title: String, image: UIImage)  {
+        super.init()
+        self.coordinate = coordinate
+        self.image = image
+        self.title = title
+    }
 }
