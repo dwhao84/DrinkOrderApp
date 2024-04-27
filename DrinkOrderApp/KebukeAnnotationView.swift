076ce92c13aa885de99384bd37da65eab6361333
Daweihao84 <dwsamurai84@gmail.com>
2024-04-28 01:36:26 +0800

    Build the Kebuke AnnotationView for showing Kebuke's icon in mapView.

--- DrinkOrderApp/KebukeAnnotationView.swift
+++ DrinkOrderApp/KebukeAnnotationView.swift
@@ -1,20 +1,59 @@
-//
-//  KebukeAnnotationView.swift
-//  DrinkOrderApp
-//
-//  Created by Dawei Hao on 2024/4/28.
-//
-
 import UIKit
+import MapKit
 
 class KebukeAnnotationView: MKAnnotationView {
+    
+    static let identifier = "KebukeAnnotationView"
+    
+    private var kebukeImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private var storeNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .darkGray
+        label.font = UIFont.boldSystemFont(ofSize: 10)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+        frame = CGRect(x: 0, y: 0, width: 60, height: 60)  // Adjusted for better visibility
+        centerOffset = CGPoint(x: 0, y: -frame.size.height / 2)
+
+        canShowCallout = true
+        setupUI()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        addSubview(kebukeImageView)
+        addSubview(storeNameLabel)
+        
+        NSLayoutConstraint.activate([
+            kebukeImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
+            kebukeImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            kebukeImageView.widthAnchor.constraint(equalToConstant: 40),
+            kebukeImageView.heightAnchor.constraint(equalToConstant: 40),
+            
+            storeNameLabel.topAnchor.constraint(equalTo: kebukeImageView.bottomAnchor, constant: 5),
+            storeNameLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            storeNameLabel.widthAnchor.constraint(equalTo: self.widthAnchor)
+        ])
+    }
+    
+    func configure(with image: UIImage?, title: String) {
+        kebukeImageView.image = image
+        storeNameLabel.text = title
+    }
 }
