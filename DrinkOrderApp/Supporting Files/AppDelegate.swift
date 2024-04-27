99391e9a460ac8b6b721cfac52246b332838c3bd
Daweihao84 <dwsamurai84@gmail.com>
2024-04-28 01:37:05 +0800

    For showing navigationController's backBtn's color.

--- DrinkOrderApp/Supporting Files/AppDelegate.swift
+++ DrinkOrderApp/Supporting Files/AppDelegate.swift
@@ -12,6 +12,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Ask the UINavigationBar appearance's tintColor
+        UINavigationBar.appearance().tintColor = Colors.white
+        
         return true
     }
 
