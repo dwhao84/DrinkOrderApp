63dc45e8c8008ee202613ebc3e99d063e3fc9293
Daweihao84 <dwsamurai84@gmail.com>
2024-04-28 22:33:53 +0800

    Add kebukeDarkBlueWithAlpha.

--- DrinkOrderApp/Constants/ColorConstants.swift
+++ DrinkOrderApp/Constants/ColorConstants.swift
@@ -9,9 +9,10 @@ import UIKit
 
 struct Colors {
     // Kebuke Colors:
-    static let kebukeDarkBlue: UIColor  = UIColor(red: 16/255, green: 45/255, blue: 61/255, alpha: 1)
-    static let kebukeLightBlue: UIColor = UIColor(red: 23/255, green: 61/255, blue: 80/255, alpha: 1)
-    static let kebukeBrown: UIColor     = UIColor(red: 182/255, green: 151/255, blue: 100/255, alpha: 1)
+    static let kebukeDarkBlue: UIColor  = UIColor(red: 4/255, green: 44/255, blue: 56/255, alpha: 1)
+    static let kebukeLightBlue: UIColor = UIColor(red: 0/255, green: 62/255, blue: 82/255, alpha: 1)
+    static let kebukeDarkBlueWithAlpha: UIColor = UIColor(red: 4/255, green: 44/255, blue: 56/255, alpha: 0.6)
+    static let kebukeBrown: UIColor     = UIColor(red: 188/255, green: 150/255, blue: 92/255, alpha: 1)
     
     // System Colors:
     static let white: UIColor           = UIColor.white
