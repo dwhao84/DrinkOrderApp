e42b61f8000ec9c59bc5d21ce289d884c3f0d9b8
Daweihao84 <dwsamurai84@gmail.com>
2024-04-30 01:10:03 +0800

    Create second RegisterBtn for different class.

--- DrinkOrderApp/Custom Buttons/SecRegisterButton.swift
+++ DrinkOrderApp/Custom Buttons/SecRegisterButton.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class SecRegisterButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
 
+    private func commonInit() {
+        
+        var config                 = UIButton.Configuration.filled()
+        config.baseBackgroundColor = Colors.kebukeBrown
+        var container = AttributeContainer()
+        container.font = UIFont.boldSystemFont(ofSize: 18)
+        config.attributedTitle = AttributedString("Register", attributes: container)
+        config.titleAlignment      = .center
+        config.cornerStyle         = .large
+        self.configuration         = config
+
+        configurationUpdateHandler = { button in
+            button.alpha = button.isHighlighted ? 0.5 : 1
+        }
+    }
+}
+
+#Preview {
+    let registerBtn = SecRegisterButton()
+    return registerBtn
 }
