8f1daf60274701db02ad8f8c1ef0092f3b96912f
Daweihao84 <dwsamurai84@gmail.com>
2024-05-03 23:54:15 +0800

    Adjust the spacing for constants.

--- DrinkOrderApp/Constants/ColorConstants.swift
+++ DrinkOrderApp/Constants/ColorConstants.swift
@@ -9,21 +9,21 @@ import UIKit
 
 struct Colors {
     // Kebuke Colors:
-    static let kebukeDarkBlue: UIColor  = UIColor(red: 4/255, green: 44/255, blue: 56/255, alpha: 1)
-    static let kebukeLightBlue: UIColor = UIColor(red: 0/255, green: 62/255, blue: 82/255, alpha: 1)
+    static let kebukeDarkBlue: UIColor          = UIColor(red: 4/255, green: 44/255, blue: 56/255, alpha: 1)
+    static let kebukeLightBlue: UIColor         = UIColor(red: 0/255, green: 62/255, blue: 82/255, alpha: 1)
     static let kebukeDarkBlueWithAlpha: UIColor = UIColor(red: 4/255, green: 44/255, blue: 56/255, alpha: 0.6)
-    static let kebukeBrown: UIColor     = UIColor(red: 188/255, green: 150/255, blue: 92/255, alpha: 1)
+    static let kebukeBrown: UIColor             = UIColor(red: 188/255, green: 150/255, blue: 92/255, alpha: 1)
     
     // System Colors:
-    static let white: UIColor           = UIColor.white
-    static let black: UIColor           = UIColor.black
-    static let darkGray: UIColor        = UIColor.darkGray
-    static let lightGray: UIColor       = UIColor.lightGray
-    static let systemGray2: UIColor     = UIColor.systemGray2
-    static let systemGray3: UIColor     = UIColor.systemGray3
-    static let systemGray4: UIColor     = UIColor.systemGray4
-    static let systemGray5: UIColor     = UIColor.systemGray5
-    static let systemGray6: UIColor     = UIColor.systemGray6
+    static let white: UIColor       = UIColor.white
+    static let black: UIColor       = UIColor.black
+    static let darkGray: UIColor    = UIColor.darkGray
+    static let lightGray: UIColor   = UIColor.lightGray
+    static let systemGray2: UIColor = UIColor.systemGray2
+    static let systemGray3: UIColor = UIColor.systemGray3
+    static let systemGray4: UIColor = UIColor.systemGray4
+    static let systemGray5: UIColor = UIColor.systemGray5
+    static let systemGray6: UIColor = UIColor.systemGray6
     
     static let clear: UIColor     = UIColor.clear
 }
