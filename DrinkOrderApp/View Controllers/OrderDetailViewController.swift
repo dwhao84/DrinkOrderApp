9e4c9e10355ff2ca5a020c91b10d65d9f1899809
Dawei Hao <dwsamurai84@gmail.com>
2024-06-06 00:08:50 +0800

    Rename the component: productImageView to drinksImageView, and add drinkNameLabel, drinkDescriptionLabel, sugarLevelLabel, sugarLevelPickerView, iceLevelLabel.

--- DrinkOrderApp/View Controllers/OrderDetailViewController.swift
+++ DrinkOrderApp/View Controllers/OrderDetailViewController.swift
@@ -6,8 +6,19 @@
 //
 
 import UIKit
+import Kingfisher
 
 class OrderDetailViewController: UIViewController {
+    
+    let cupSize: [String] = ["中杯 M", "大杯 L"]
+    let sugarLevel: [String] = ["正常糖", "少糖", "半糖", "微糖", "二分糖", "一分糖", "無糖"]
+    let iceLevel: [String] = ["正常冰", "少冰", "微冰", "去冰", "完全去冰", "常溫", "溫", "熱"]
+    
+    
+    // MARK: - Passing Data:
+    var drinksName: String?
+    var drinksDescription: String?
+    var drinksImageURL: String?
     
     var tableView: UITableView = {
         let tableView: UITableView = UITableView()
@@ -17,12 +28,57 @@ class OrderDetailViewController: UIViewController {
     }()
 
     // Show the product image at the top.
-    var productImageView: UIImageView = {
+    let drinksImageView: UIImageView = {
         let imageView: UIImageView = UIImageView()
-        imageView.image = Images.banner03
+        imageView.image = Images.photoFill
         imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = Colors.kebukeLightBlue
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
+    } ()
+    
+    let drinkNameLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = Colors.darkGray
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    } ()
+    
+    let drinkDescriptionLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "Drink Description Label"
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = Colors.darkGray
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    } ()
+    
+    let sugarLevelLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "Sugar Level 糖量"
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = Colors.darkGray
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    } ()
+    
+    var sugarLevelPickerView: UIPickerView = {
+        let pickerView: UIPickerView = UIPickerView()
+        return pickerView
+    } ()
+    
+    let iceLevelLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "Ice Level 冰量"
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = Colors.darkGray
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     } ()
     
     var scrollView: UIScrollView = {
@@ -38,18 +94,31 @@ class OrderDetailViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         setupUI()
+        
+        // MARK: - Update UI
+        
+        guard let url = drinksImageURL else {
+            print("Unable to get drinksURL")
+            return
+        }
+        
+        drinkNameLabel.text        = drinksName
+        drinkDescriptionLabel.text = drinksDescription
+        let drinksImageUrl = URL(string: url)
+        drinksImageView.kf.setImage(with: drinksImageUrl)
     }
     
     func setupUI () {
         setNavigationView()
-        view.addSubview(productImageView)
+        view.addSubview(drinksImageView)
         addConstraints()
         self.view.backgroundColor = Colors.white
     }
     
 
     func setNavigationView () {
-        self.navigationItem.title = "飲料名稱"
+        self.navigationItem.title = drinksName
+        print("\(String(describing: drinksName))")
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = Colors.kebukeLightBlue
         appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
@@ -59,12 +128,12 @@ class OrderDetailViewController: UIViewController {
     
     func addConstraints () {
         scrollView.delegate = self
-        productImageView.translatesAutoresizingMaskIntoConstraints = false
+        drinksImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            productImageView.topAnchor.constraint(equalTo: view.topAnchor),
-            productImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            productImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            productImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9)
+            drinksImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            drinksImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            drinksImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            drinksImageView.heightAnchor.constraint(equalTo: drinksImageView.widthAnchor, multiplier: 0.57)
         ])
     }
 }
