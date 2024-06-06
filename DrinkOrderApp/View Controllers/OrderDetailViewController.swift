f968c9ef12be25a0de8265d9b21b22a0e3cb4010
Dawei Hao <dwsamurai84@gmail.com>
2024-06-07 00:34:39 +0800

    Add TextField into the detailVC, and show the pickerView in textField.

--- DrinkOrderApp/View Controllers/OrderDetailViewController.swift
+++ DrinkOrderApp/View Controllers/OrderDetailViewController.swift
@@ -20,17 +20,11 @@ class OrderDetailViewController: UIViewController {
     var drinksDescription: String?
     var drinksImageURL: String?
     
-    var tableView: UITableView = {
-        let tableView: UITableView = UITableView()
-        tableView.backgroundColor = Colors.kebukeDarkBlue
-        tableView.isScrollEnabled = true
-        return tableView
-    }()
-
+    
     // Show the product image at the top.
     let drinksImageView: UIImageView = {
         let imageView: UIImageView = UIImageView()
-        imageView.image = Images.photoFill
+        imageView.image = Images.banner04
         imageView.contentMode = .scaleAspectFit
         imageView.tintColor = Colors.kebukeLightBlue
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -39,7 +33,7 @@ class OrderDetailViewController: UIViewController {
     
     let drinkNameLabel: UILabel = {
         let label: UILabel = UILabel()
-        label.font = UIFont.systemFont(ofSize: 15)
+        label.font = UIFont.boldSystemFont(ofSize: 20)
         label.textColor = Colors.darkGray
         label.textAlignment = .left
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -49,36 +43,99 @@ class OrderDetailViewController: UIViewController {
     let drinkDescriptionLabel: UILabel = {
         let label: UILabel = UILabel()
         label.text = "Drink Description Label"
-        label.font = UIFont.systemFont(ofSize: 15)
+        label.font = UIFont.systemFont(ofSize: 16)
         label.textColor = Colors.darkGray
         label.textAlignment = .left
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     } ()
     
+    // MARK: - Sugar Level
     let sugarLevelLabel: UILabel = {
         let label: UILabel = UILabel()
         label.text = "Sugar Level 糖量"
-        label.font = UIFont.systemFont(ofSize: 15)
+        label.font = UIFont.boldSystemFont(ofSize: 20)
         label.textColor = Colors.darkGray
         label.textAlignment = .left
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     } ()
     
-    var sugarLevelPickerView: UIPickerView = {
-        let pickerView: UIPickerView = UIPickerView()
-        return pickerView
+    let sugarTextField: UITextField = {
+        let textField: UITextField = UITextField()
+        textField.text = ""
+        textField.borderStyle = .roundedRect
+        textField.isEnabled = true
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    } ()
+        
+    let sugarStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     } ()
     
+    // MARK: - ice Level
     let iceLevelLabel: UILabel = {
         let label: UILabel = UILabel()
         label.text = "Ice Level 冰量"
-        label.font = UIFont.systemFont(ofSize: 15)
+        label.font = UIFont.boldSystemFont(ofSize: 20)
         label.textColor = Colors.darkGray
         label.textAlignment = .left
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    } ()
+    
+    let iceLevelPickerView: UIPickerView = UIPickerView()
+    let sugarLevelPickerView: UIPickerView = UIPickerView()
+    
+    let iceTextField: UITextField = {
+        let textField: UITextField = UITextField()
+        textField.text = ""
+        textField.borderStyle = .roundedRect
+        textField.isEnabled = true
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    } ()
+    
+    let iceStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    } ()
+    
+    let mainStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fill
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    } ()
+        
+    var submitBtn: UIButton = {
+        let btn: UIButton = UIButton(type: .system)
+        var config = UIButton.Configuration.filled()
+        
+        var title = AttributedString("Submit")
+        title.font = UIFont.boldSystemFont(ofSize: 20)
+        config.attributedTitle = title
+        config.cornerStyle = .large
+        config.baseBackgroundColor = Colors.kebukeLightBlue
+        config.titleAlignment = .center
+        btn.configuration = config
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
     } ()
     
     var scrollView: UIScrollView = {
@@ -91,12 +148,11 @@ class OrderDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         setupUI()
         
-        // MARK: - Update UI
-        
+        // Passing Data from HomePageVC:
         guard let url = drinksImageURL else {
             print("Unable to get drinksURL")
             return
@@ -109,38 +165,179 @@ class OrderDetailViewController: UIViewController {
     }
     
     func setupUI () {
-        setNavigationView()
-        view.addSubview(drinksImageView)
-        addConstraints()
         self.view.backgroundColor = Colors.white
+        setNavigationView()
+        addSubviews()
+        addConstraints()
+        configStackView()
+        addDelegateAndDataSource()
+        addTargets()
+        setupTextFields()
     }
     
+    func addSubviews () {
+        view.addSubview(drinksImageView)
+        view.addSubview(submitBtn)
+        view.addSubview(iceTextField)
+        view.addSubview(iceLevelLabel)
+        view.addSubview(sugarStackView)
+        view.addSubview(iceStackView)
+    }
+    
+    func addTargets () {
+        submitBtn.addTarget(self, action: #selector(submitBtnTapped), for: .touchUpInside)
+    }
+    
+    func setupTextFields () {
+        sugarTextField.inputView = sugarLevelPickerView
+        iceTextField.inputView   = iceLevelPickerView
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneBtnTapped))
+           let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+           let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelBtnTapped))
 
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+
+        sugarTextField.inputAccessoryView = toolBar
+        iceTextField.inputAccessoryView = toolBar
+    }
+    
     func setNavigationView () {
         self.navigationItem.title = drinksName
-        print("\(String(describing: drinksName))")
+        print("DEBUG PRINT: 印出產品名稱\(String(describing: drinksName))")
+        
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = Colors.kebukeLightBlue
         appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        self.navigationController?.navigationBar.standardAppearance = appearance
+        self.navigationController?.navigationBar.standardAppearance   = appearance
         self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
     
     func addConstraints () {
-        scrollView.delegate = self
-        drinksImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             drinksImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             drinksImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             drinksImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            drinksImageView.heightAnchor.constraint(equalTo: drinksImageView.widthAnchor, multiplier: 0.57)
+            drinksImageView.heightAnchor.constraint(equalTo: drinksImageView.widthAnchor, multiplier: 0.75)
         ])
+        
+        NSLayoutConstraint.activate([
+            submitBtn.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+            submitBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            submitBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            submitBtn.heightAnchor.constraint(equalToConstant: 45)
+        ])
+        
+        /// sugarStackView:
+        NSLayoutConstraint.activate([
+            sugarStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            sugarStackView.topAnchor.constraint(equalTo: drinksImageView.bottomAnchor, constant: 30),
+            sugarStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            sugarTextField.widthAnchor.constraint(equalToConstant: 200),
+            sugarTextField.heightAnchor.constraint(lessThanOrEqualToConstant: 60),
+        ])
+        
+        NSLayoutConstraint.activate([
+            iceStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            iceStackView.topAnchor.constraint(equalTo: sugarStackView.bottomAnchor, constant: 30),
+            iceStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            iceTextField.widthAnchor.constraint(equalToConstant: 200),
+            iceTextField.heightAnchor.constraint(lessThanOrEqualToConstant: 60),
+        ])
+    }
+    
+    func configStackView () {
+        sugarStackView.addArrangedSubview(sugarLevelLabel)
+        sugarStackView.addArrangedSubview(sugarTextField)
+        iceStackView.addArrangedSubview(iceLevelLabel)
+        iceStackView.addArrangedSubview(iceTextField)
+    }
+    
+    func addDelegateAndDataSource () {
+        iceTextField.delegate = self
+        sugarTextField.delegate = self
+        
+        iceLevelPickerView.delegate = self
+        iceLevelPickerView.dataSource = self
+        sugarLevelPickerView.delegate = self
+        sugarLevelPickerView.dataSource = self
+    }
+    
+    
+    @objc func submitBtnTapped (_ sender: UIButton) {
+        print("sumbit btn tapped")
+    }
+    
+    @objc func cancelBtnTapped (_ sender: UIBarButtonItem) {
+        print("cancelBtnTapped")
+        sugarTextField.resignFirstResponder()
+        iceTextField.resignFirstResponder()
+    }
+    
+    @objc func doneBtnTapped (_ sender: UIBarButtonItem) {
+        print("doneBtnTapped")
+        sugarTextField.resignFirstResponder()
+        iceTextField.resignFirstResponder()
     }
 }
 
 extension OrderDetailViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
+    }
+}
+
+extension OrderDetailViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        iceLevelPickerView.resignFirstResponder()
+        sugarLevelPickerView.resignFirstResponder()
+        return true
+    }
+}
+
+extension OrderDetailViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    // UIPickerViewDataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (pickerView == iceLevelPickerView) {
+            print(iceLevel.count)
+            return iceLevel.count
+        } else {
+            print(sugarLevel.count)
+            return sugarLevel.count
+        }
+    }
+    
+    // UIPickerViewDelegate
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (pickerView == iceLevelPickerView) {
+            iceTextField.text = iceLevel[row]
+            iceLevelPickerView.resignFirstResponder()
+        } else {
+            sugarTextField.text = sugarLevel[row]
+            sugarLevelPickerView.resignFirstResponder()
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if (pickerView == iceLevelPickerView) {
+            print(iceLevel.count)
+            return iceLevel[row]
+        } else {
+            print(sugarLevel.count)
+            return sugarLevel[row]
+        }
     }
 }
 
