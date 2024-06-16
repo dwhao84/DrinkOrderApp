a668d9dcec49fc4e56e218437643a14d5c3a81a5
Dawei Hao <dwsamurai84@gmail.com>
2024-06-16 23:39:53 +0800

    Create the CRUD method for get / post / patch / delete.

--- DrinkOrderApp/Network Manager/NetworkManager.swift
+++ DrinkOrderApp/Network Manager/NetworkManager.swift
@@ -27,7 +27,7 @@ class NetworkManager {
     static let application_json: String = "application/json"
     
     
-    // MARK: - Result type:
+    // MARK: - Result type for GET:
     enum Result<drinks, Error: Swift.Error>  {
         case success(drinks)
         case failure(Error)
@@ -39,8 +39,9 @@ class NetworkManager {
         case unexpectedStatusCode
         case noDataReceived
         case decodeError
+        case custom(String)
+        case encodeError
     }
-    
     
     // MARK: - GET orders data
     func getOrdersData(url: String, completion: @escaping( Result<Kebuke, NetworkError>) -> Void) {
@@ -90,22 +91,133 @@ class NetworkManager {
         }.resume()
     }
     
+    var orders: [Order] = []
+    
+    // MARK: - Result type for POST and PATCH:
+    enum CreateOrder<Order, Error> {
+        case success(Order)
+        case failure(Error)
+    }
+    
+//    enum PostNetworkError: Error {
+//        case invalidUrl
+//        case encodeError
+//        case noData
+//        case decodeError
+//        case custom(String)
+//    }
+    
     
     // MARK: - POST orders
-    func postOrdersData () {
+    func postOrdersData(order: Order, completion: @escaping(Result<Order, NetworkError>) -> Void) {
+        guard let url = URL(string: apiUrl) else {
+            fatalError()
+        }
         
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue(NetworkManager.application_json, forHTTPHeaderField: NetworkManager.contentType)
+        
+        do {
+            let jsonData = try JSONEncoder().encode(orders)
+            request.httpBody = jsonData
+            print(jsonData)
+        } catch {
+            completion(.failure(.encodeError))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            // Make sure no error.
+            if let error = error {
+                completion(.failure(.custom(error.localizedDescription)))
+                return
+            }
+            
+            // Make sure have data.
+            guard let data = data else {
+                completion(.failure(.noDataReceived))
+                return
+            }
+            
+            do {
+                let createOrder = try JSONDecoder().decode(Order.self, from: data)
+                completion(.success(createOrder))
+            } catch {
+                completion(.failure(.decodeError))
+            }
+        }
+        task.resume()
     }
     
     
     // MARK: - PATCH orders
-    func patchOrdersData () {
+    func patchOrdersData (orderId: String, updatedOrder: Order, completion: @escaping (Result<Order, NetworkError>) -> Void) {
+        let patchUrl: String = "\(apiUrl)/\(orderId)"
         
+        guard let url = URL(string: patchUrl) else {
+            fatalError()
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PATCH"
+        request.setValue(NetworkManager.application_json, forHTTPHeaderField: NetworkManager.contentType)
+        
+        do {
+            let jsonData = try JSONEncoder().encode(updatedOrder)
+            request.httpBody = jsonData
+        } catch {
+            completion(.failure(.encodeError))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(.custom(error.localizedDescription)))
+            }
+            
+            guard let data = data else {
+                completion(.failure(.noDataReceived))
+                return
+            }
+            
+            do {
+                let task = try JSONDecoder().decode(Order.self, from: data)
+                completion(.success(updatedOrder))
+            } catch {
+                completion(.failure(.decodeError))
+                return
+            }
+        }
+        task.resume()
     }
     
     
     // MARK: - DELETE orders
-    func deleteOrdersData () {
+    func deleteOrdersData (orderId: String, completion: @escaping (Result<Void, NetworkError>) -> Void) {
+        let deleteUrl = "\(apiUrl)/\(orderId)"
         
+        guard let url = URL(string: deleteUrl) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        request.setValue(NetworkManager.application_json, forHTTPHeaderField: NetworkManager.contentType)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(.custom(error.localizedDescription)))
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                completion(.success(()))
+            } else {
+                completion(.failure(.unexpectedStatusCode))
+            }
+        }
+        task.resume()
     }
     
 }
