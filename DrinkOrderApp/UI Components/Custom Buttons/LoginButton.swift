e31ab1962440cf166a13352692677e13fe5ef35e
Dawei Hao <dwsamurai84@gmail.com>
2024-06-18 22:29:44 +0800

    Adjust cornerStyle.

--- DrinkOrderApp/UI Components/Custom Buttons/LoginButton.swift
+++ DrinkOrderApp/UI Components/Custom Buttons/LoginButton.swift
@@ -27,7 +27,7 @@ class LoginButton: UIButton {
         container.font = UIFont.boldSystemFont(ofSize: 18)
         config.attributedTitle = AttributedString("Login", attributes: container)
         config.titleAlignment      = .center
-        config.cornerStyle         = .large
+        config.cornerStyle         = .capsule
         self.configuration         = config
 
         configurationUpdateHandler = { button in
---
+++ DrinkOrderApp/UI Components/Custom TextFields/PasswordTextField.swift
@@ -0,0 +1,20 @@
+//
+//  PasswordTextField.swift
+//  DrinkOrderApp
+//
+//  Created by Dawei Hao on 2024/6/18.
+//
+
+import UIKit
+
+class PasswordTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
