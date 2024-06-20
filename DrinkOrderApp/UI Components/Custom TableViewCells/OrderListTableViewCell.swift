0ebbe8bcb043e631929bac530d6f633ddaec9565
Dawei Hao <dwsamurai84@gmail.com>
2024-06-20 23:13:51 +0800

    Adjust the auto-layout.

--- DrinkOrderApp/UI Components/Custom TableViewCells/OrderListTableViewCell.swift
+++ DrinkOrderApp/UI Components/Custom TableViewCells/OrderListTableViewCell.swift
@@ -11,28 +11,38 @@ class OrderListTableViewCell: UITableViewCell {
     
     static let identifier: String = "OrderListTableViewCell"
     
+    let imageViewWidth: CGFloat = 150.0
+    
     let drinksImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = Images.banner02
         imageView.contentMode = .scaleAspectFill
-        imageView.layer.cornerRadius = 10
         imageView.clipsToBounds = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
-    let drinksTitleLabel: UILabel = {
+    let userNameLabel: UILabel = {
         let label = UILabel()
-        label.text = "熟成紅茶"
+        label.text = "Loading Name..."
         label.textColor = Colors.white
         label.font = UIFont.boldSystemFont(ofSize: 18)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let drinksDescriptionLabel: UILabel = {
+    let drinksTitleLabel: UILabel = {
         let label = UILabel()
-        label.text = "帶有濃穩果香的經典紅茶"
+        label.text = "Loading drinks..."
+        label.textColor = Colors.white
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let toppingContentLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Loading 配料..."
         label.numberOfLines = 2
         label.textColor = Colors.lightGray
         label.font = UIFont.systemFont(ofSize: 14)
@@ -42,18 +52,9 @@ class OrderListTableViewCell: UITableViewCell {
     
     let drinksPriceLabel: UILabel = {
         let label = UILabel()
-        label.text = "$35"
+        label.text = "Loading..."
         label.textColor = Colors.white
         label.font = UIFont.systemFont(ofSize: 18)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    let orderNameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "顯示狀態"
-        label.textColor = Colors.white
-        label.font = UIFont.boldSystemFont(ofSize: 15)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -64,62 +65,82 @@ class OrderListTableViewCell: UITableViewCell {
         return stepper
     }()
     
+    let contentStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        stackView.alignment = .leading
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    let mainStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupViews()
+
+        configStackView()
         setupConstraints()
+    
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 
-    
-    private func setupViews() {
-        contentView.backgroundColor = Colors.kebukeLightBlue
-        contentView.addSubview(drinksImageView)
-        contentView.addSubview(drinksTitleLabel)
-        contentView.addSubview(drinksDescriptionLabel)
-        contentView.addSubview(drinksPriceLabel)
-        contentView.addSubview(orderNameLabel)
-        contentView.addSubview(customStepper)
+    private func setupConstraints() {
+        drinksImageView.layer.cornerRadius = imageViewWidth / 2
+        self.backgroundColor = Colors.kebukeLightBlue
+        self.addSubview(drinksImageView)
+        self.addSubview(customStepper)
+        self.addSubview(contentStackView)
+        self.addSubview(mainStackView)
+        
+        NSLayoutConstraint.activate([
+            // drinksImageView
+            drinksImageView.widthAnchor.constraint(equalToConstant: imageViewWidth),
+            drinksImageView.heightAnchor.constraint(equalTo: drinksImageView.widthAnchor, multiplier: 1),
+            drinksImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            drinksImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            
+            // contentStackView
+            contentStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -15),
+            contentStackView.leadingAnchor.constraint(equalTo: drinksImageView.trailingAnchor, constant: 20),
+            
+            // Stepper
+            customStepper.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+            customStepper.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10)
+        ])
     }
     
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([
-            drinksImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
-            drinksImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-            drinksImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
-            drinksImageView.widthAnchor.constraint(equalToConstant: 150),
-            
-            drinksTitleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
-            drinksTitleLabel.leadingAnchor.constraint(equalTo: drinksImageView.trailingAnchor, constant: 20),
-            drinksTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
-            
-            drinksDescriptionLabel.topAnchor.constraint(equalTo: drinksTitleLabel.bottomAnchor, constant: 10),
-            drinksDescriptionLabel.leadingAnchor.constraint(equalTo: drinksTitleLabel.leadingAnchor),
-            drinksDescriptionLabel.trailingAnchor.constraint(equalTo: drinksTitleLabel.trailingAnchor),
-            
-            drinksPriceLabel.topAnchor.constraint(equalTo: drinksDescriptionLabel.bottomAnchor, constant: 10),
-            drinksPriceLabel.leadingAnchor.constraint(equalTo: drinksTitleLabel.leadingAnchor),
-            drinksPriceLabel.trailingAnchor.constraint(equalTo: drinksTitleLabel.trailingAnchor),
-            
-            orderNameLabel.topAnchor.constraint(equalTo: drinksPriceLabel.bottomAnchor,
-                                               constant: 10),
-            orderNameLabel.leadingAnchor.constraint(equalTo: drinksTitleLabel.leadingAnchor),
-            orderNameLabel.trailingAnchor.constraint(equalTo: drinksTitleLabel.trailingAnchor),
-                        
-            customStepper.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -45),
-            customStepper.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
-            customStepper.heightAnchor.constraint(equalToConstant: 30)
-        ])
+    func configStackView () {
+        contentStackView.addArrangedSubview(userNameLabel)
+        contentStackView.addArrangedSubview(drinksTitleLabel)
+        contentStackView.addArrangedSubview(toppingContentLabel)
+        contentStackView.addArrangedSubview(drinksPriceLabel)
+
+        mainStackView.addArrangedSubview(drinksImageView)
+        mainStackView.addArrangedSubview(contentStackView)
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
         customStepper.value = 0
     }
+    
+    @objc func customStepperTapped() {
+        
+    }
+    
 }
 
 
