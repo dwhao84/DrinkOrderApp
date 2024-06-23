e2abacde0dbe8c76ef3e874aa5c38478823326d1
Dawei Hao <dwsamurai84@gmail.com>
2024-06-24 00:37:31 +0800

    Adjust the content and using check function to check every situation is correct or not.

--- DrinkOrderApp/View Controllers/Login VCs/RegisterViewController.swift
+++ DrinkOrderApp/View Controllers/Login VCs/RegisterViewController.swift
@@ -228,29 +228,115 @@ class RegisterViewController: UIViewController {
         ])
     }
     
+    // MARK: - Check Name correction
+    func checkNameCorrection (_ userName: String) -> NameCheck {
+        let textLength = Int(nameTextField.text?.count ?? 0)
+        
+        if textLength < 1 && textLength > 10 {
+            print("""
+                   DEBUG PRINT: User Name's text length is not correct
+                   \(textLength)
+                """)
+            return .lackTextlength
+        } else if textLength == 0 {
+            print("DEBUG PRINT: User name is empty")
+            return .empty
+        } else {
+            print("DEBUG PRINT: User name is valid")
+            return .valid
+        }
+    }
+    
+    // MARK: - Check Password correction
+    func checkPasswordCorrection(_ password: String) -> PasswordCheck {
+        let tenCommonPasswords = ["123456", "123456789", "qwerty", "password", "12345678", "111111", "iloveyou", "1q2w3e", "123123", "password1"]
+        let digits = "0123456789"
+        let punctuation = "!@#$%^&*(),.<>;'`~[]{}\\|/?_-+= "
+        let textLength = Int(passwordTextField.text?.count ?? 0)
+        
+        if tenCommonPasswords.contains(password) {
+            print("DEBUG PRINT: Using ten common passwords")
+            return .containsCommonPassword
+            
+        } else if textLength == 0 {
+            print("DEBUG PRINT: Password is empty")
+            return .empty
+            
+        } else if !password.contains(where: { digits.contains($0) }) {
+            print("DEBUG PRINT: Password is missing digits")
+            return .lacksDigits
+            
+        } else if !password.contains(where: { punctuation.contains($0) }) {
+            print("DEBUG PRINT: Password is uncorrect")
+            return.lacksPunctuation
+            
+        } else if textLength > 16 && textLength < 9 {
+            
+            print("""
+                 DEBUG PRINT: Password's text length is not correct
+                \(textLength)
+                """)
+            return .lackTextLength
+        } else {
+            print("DEBUG PRINT: Password is valid")
+            return .valid }
+    }
+    
+    // MARK: - Check Mail Correction:
+    func checkEmailCorrection(_ email: String) -> EmailCheck {
+        let emailDomains = [
+            "gmail.com", "yahoo.com", "hotmail.com", "aol.com", "hotmail.co.uk",
+            "hotmail.fr", "msn.com", "yahoo.fr", "wanadoo.fr", "orange.fr",
+            "comcast.net", "yahoo.co.uk", "yahoo.com.br", "yahoo.co.in", "live.com",
+            "rediffmail.com", "free.fr", "gmx.de", "web.de", "yandex.ru",
+            "ymail.com", "libero.it", "outlook.com", "uol.com.br", "bol.com.br",
+            "mail.ru", "cox.net", "hotmail.it", "sbcglobal.net", "sfr.fr",
+            "live.fr", "verizon.net", "live.co.uk", "googlemail.com", "yahoo.es",
+            "ig.com.br", "live.nl", "bigpond.com", "terra.com.br", "yahoo.it",
+            "neuf.fr", "yahoo.de", "alice.it", "rocketmail.com", "att.net",
+            "laposte.net", "facebook.com", "bellsouth.net", "yahoo.in", "hotmail.es",
+            "charter.net", "yahoo.ca", "yahoo.com.au", "rambler.ru", "hotmail.de",
+            "tiscali.it", "shaw.ca", "yahoo.co.jp", "sky.com", "earthlink.net",
+            "optonline.net", "freenet.de", "t-online.de", "aliceadsl.fr", "virgilio.it",
+            "home.nl", "qq.com", "telenet.be", "me.com", "yahoo.com.ar",
+            "tiscali.co.uk", "yahoo.com.mx", "voila.fr", "gmx.net", "mail.com",
+            "planet.nl", "tin.it", "live.it", "ntlworld.com", "arcor.de",
+            "yahoo.co.id", "frontiernet.net", "hetnet.nl", "live.com.au", "yahoo.com.sg",
+            "zonnet.nl", "club-internet.fr", "juno.com", "optusnet.com.au", "blueyonder.co.uk",
+            "bluewin.ch", "skynet.be", "sympatico.ca", "windstream.net", "mac.com",
+            "centurytel.net", "chello.nl", "live.ca", "aim.com", "bigpond.net.au"
+        ]
+        let at = "@"
+        let punctuation = "!@#$%^&*(),.<>;'`~[]{}\\|/?_-+= "
+        
+        if !email.contains(where: { punctuation.contains( $0 )}) {
+            print("DEBUG PRINT: E-mail is not the correct entering")
+            return .lacksPunctuation
+        } else if !emailDomains.contains(emailDomains) {
+            print("DEBUG PRINT: E-mail is entering wrong domain")
+            return .invalidDomain
+        } else if !email.contains(at) {
+            print("DEBUG PRINT: E-mail is missing @")
+            return .lackAt
+        } else {
+            print("DEBUG PRINT: E-mail is vaild")
+            return .valid }
+    }
+    
     // MARK: - Action:
     @objc func registerButtonTapped(_ sender: UIButton)  {
-        
         let enteredName = nameTextField.text ?? ""
         let enteredMail = mailTextField.text ?? ""
         let enteredPassword = passwordTextField.text ?? ""
         
-        if enteredName.isEmpty {
-            print("enteredName is empty")
-        }
-        
-        if enteredMail.isEmpty {
-            print("enteredMail is empty")
-        }
-        
-        if (enteredPassword.count == 0) {
-            print("")
-            return
-        }
+        let nameResult = checkNameCorrection(enteredName)
+        let emailResult = checkEmailCorrection(enteredMail)
+        let passwordResult = checkPasswordCorrection(enteredPassword)
         
         
         
-        
+        let loginVC = LoginViewController()
+        self.present(loginVC, animated: true)
         
         
     }
@@ -272,31 +358,30 @@ class RegisterViewController: UIViewController {
 // MARK: - Extension:
 extension RegisterViewController: UITextFieldDelegate {
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        print("textFieldShouldEndEditing")
+        print("textField Should End Editing")
         textField.resignFirstResponder()
         return true
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print("textFieldShouldReturn")
+        print("textField Should Return")
         textField.resignFirstResponder()
         return true
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        print("textFieldDidBeginEditing")
+        print("textField Did Begin Editing")
         textField.becomeFirstResponder()
     }
     
     func textFieldDidChangeSelection(_ textField: UITextField) {
-        print("textFieldDidChangeSelection")
+        print("textField Did Change Selection")
         print("DEBUG PRINT: \(textField.text ?? "")")
         textField.becomeFirstResponder()
     }
     
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        print("textFieldShouldClear")
-        
+        print("textField Should Clear")
         textField.text = ""
         return true
     }
