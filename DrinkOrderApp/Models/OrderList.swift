a3f4629a9fb77cb54b2c501f3770d86458811b5f
Dawei Hao <dwsamurai84@gmail.com>
2024-07-13 00:38:04 +0800

    Adjust the Order List model.

--- DrinkOrderApp/Models/OrderList.swift
+++ DrinkOrderApp/Models/OrderList.swift
@@ -5,4 +5,37 @@
 //  Created by Dawei Hao on 2024/7/12.
 //
 
-import Foundation
+import UIKit
+
+struct OrderResponse: Codable {
+    let records: [CustomerOrder]
+}
+
+struct CustomerOrder: Codable {
+    let fields: CustomerOrderFields
+}
+
+// MARK: - CustomerOrderFields Model
+
+struct CustomerOrderFields: Codable {
+    let userName: String?
+    let drinkName: String?
+    let cupSize: String?
+    let sugarLevel: String?
+    let iceLevel: String?
+    let topping: String?
+    let qty: String?
+    let price: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case userName   = "User name"
+        case drinkName  = "Drink name"
+        case cupSize    = "Cup Size"
+        case sugarLevel = "Sugar Level"
+        case iceLevel   = "Ice Level"
+        case topping    = "Topping"
+        case qty        = "Qty"
+        case price      = "Price"
+    }
+}
+
