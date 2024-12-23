8e45b9cb72653f456d1ec46695a671ddce387d5e
Dawei Hao <dwsamurai84@gmail.com>
2024-12-23 21:05:29 +0800

    Update content.

---
+++ DrinkOrderApp/Constants/APIConfig.swift
@@ -0,0 +1,15 @@
+//
+//  APOConfig.swift
+//  DrinkOrderApp
+//
+//  Created by Dawei Hao on 2024/11/20.
+//
+
+import UIKit
+
+struct APIConfig {
+    static let airtableApiKey = "Bearer patxAQx4KLgwEsh8O.28a883dd0c29a3920aee1cc069fc876738b14186ec8ec2dd07cc762b70497e0c"
+    static let favqsToken = "Token token a7c01d0f0e393ecf59c17dd7a66b193a"
+    static let baseUrl = "https://api.airtable.com/v0/appS5I28H2YO3bJzv"
+    static let favqsUrl = "https://favqs.com/api/"
+}
